6058d418721c9d0ed2b0c8fa5b09a239a9dcef5e
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-02-15 21:00:14 +0300

    мучаю рендеринг

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,63 @@ function handleFormSubmit(evt) {
 
 formElement.addEventListener('submit', handleFormSubmit);
 
+/* --Обработка попапа места-- */
+// let popup = document.querySelector('.popup');
+const buttonAdd = document.querySelector('.profile__add-button');
+
+// let profileName = document.querySelector('.profile__name');
+// let profileAbout = document.querySelector('.profile__about');
+
+const placeNameInput = document.querySelector('.place-name-input');
+const placeImageInput = document.querySelector('.place-image-input');
+
+/* --Рендеринг карточек-- */
+const defoltElements = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const elementsList = document.querySelector('.elements__list');
+// const form = document.querySelector('.') тут добавляем форму добавления мест
+const cardTemplate = document.getElementById('user-card');
+
+const getElement = (element) => {
+    const newElement = cardTemplate.content.cloneNode(true);
+
+    const newElementTitle = newElement.querySelector('.elements__title');
+    const newElementImage = newElement.querySelector('.elements__image').src;
+    newElementTitle.textContent = element.name;
+    newElementImage.src = element.link;
+
+    elementsList.append(newElement);
+};
+
+const renderElement = (wrap, element) => {
+    wrap.append(getElement(element));
+};
+
+defoltElements.forEach((element) => {
+    renderElement(elementsList, element);
+});
